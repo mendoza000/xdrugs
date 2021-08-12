33a22bd6fb4185e2097c4f1713ad7c6d71da0fe6
mendoza000 <omarmendoza.alv25@gmail.com>
2021-08-12 19:23:06 -0400

    bases del proyecto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/normalize.css'
+import './styles.css'
+
+import { menuEvent } from './js/helpers/menu-events' 
+import { writeHeader } from './js/helpers/write-header'
+
+/*document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`*/
+/*writeHeader()
+*/menuEvent()
